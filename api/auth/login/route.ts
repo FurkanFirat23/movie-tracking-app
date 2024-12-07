31747f947139249fa29ec8cb3ffa6bc5650e2ea0
Furkan Fırat <furkan.0096@gmail.com>
2024-12-07 17:12:59 +0300

    to protect the user profile endpoint, allowing only logged-in userts to access their data

--- api/auth/login/route.ts
+++ api/auth/login/route.ts
@@ -1,19 +1,30 @@
-import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from 'next';
+import User from '../../../models/User';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 
-export async function POST(request: Request) {
-  const body = await request.json();
-  const { username, password } = body;
+const login = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'Kullanıcı bulunamadı' });
+    }
 
-  if (username === "test" && password === "password") {
-    return NextResponse.json(
-      { message: "Login successful" },
-      {
-        headers: {
-          "Set-Cookie": "token=valid_token; HttpOnly; Path=/;",
-        },
-      }
-    );
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Geçersiz şifre' });
+    }
+
+    // JWT token oluşturuluyor
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, {
+      expiresIn: '1h',
+    });
+
+    return res.status(200).json({ token }); // Token istemciye döndürülüyor
+  } catch (error) {
+    return res.status(500).json({ message: 'Sunucu hatası' });
   }
+};
 
-  return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
-}
+export default login;
